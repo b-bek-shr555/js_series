9b92b7ad63c1552b04f1f304019ebdc591fd2734
Bibek Shrestha <129869974+b-bek-shr555@users.noreply.github.com>
2024-01-16 12:18:12 +0000

    Configed

---
+++ 01_Basics/Numbers & Maths .js
@@ -0,0 +1,40 @@
+
+// ==================================  Numbers =================================================  //
+
+let f_num = new Number (20435450);
+let num = 232.7888;
+// console.log(f_num);
+// console.log(f_num.valueOf());
+// console.log(f_num.toString());
+// console.log(f_num.toString().split());
+// console.log(f_num.toString().length);
+// console.log(f_num.toFixed(2));
+// console.log(f_num.toLocaleString('en-IN'));
+// console.log(f_num.toExponential(2));
+// console.log(num.toPrecision(3));
+
+
+// ==================================  Maths =================================================  //
+// console.log(Math.abs(-4)); // (Abs Function) Turns Negative to Positive Number // 
+// console.log(Math.PI); // Gives Pi Values  = Property // 
+// console.log(Math.round(3.42)); // (Round Function) Round Up The value  // 
+// console.log(Math.ceil(4.2)); // (Ceiling Function) Upper Value //
+// console.log(Math.floor(16.9)); // (Floor Function) lowest value //
+// console.log(Math.sqrt()); // Square Function //
+// console.log(Math.pow(8,3)); // Power Function //
+// console.log(Math.max(23,5,565,423,122,3445,3434)); // Max Function //
+// console.log(Math.min(23,5,565,423,122,3445,3434)); // Min Function //
+
+// console.log(Math.random()); // 0 - 1 values //
+// console.log(Math.random()*10); // shifts 1 number before the decimal //
+// console.log(Math.floor(Math.random()*10 )+ 1); // shifts 1 number before the decimal and gives number from 1 t0 10 removes 0 //
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) +min );
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
